878051652b114ce497fb579f93ab2a25022ed5b1
marc1107 <m.bohner99@gmail.com>
2022-10-13 18:48:04 +0200

    Fixed Warnings

--- src/main/scala/Reversi.scala
+++ src/main/scala/Reversi.scala
@@ -4,11 +4,11 @@ object Reversi{
     println(mesh())
   }
 
-  val eol = sys.props("line.separator")
-  def bar(cellWidth:Int=3, cellNum:Int=8) =
+  val eol: String = sys.props("line.separator")
+  def bar(cellWidth:Int=3, cellNum:Int=8): String =
     ("+" + "-" * cellWidth) * cellNum + "+" + eol
-  def cells(cellWidth:Int=3, cellNum:Int=8) =
+  def cells(cellWidth:Int=3, cellNum:Int=8): String =
     ("|" + " " * cellWidth) * cellNum + "+" + eol
-  def mesh(cellWidth:Int=3, cellNum:Int=8) =
+  def mesh(cellWidth:Int=3, cellNum:Int=8): String =
     (bar(cellWidth, cellNum) + cells(cellWidth, cellNum)) * cellNum + bar(cellWidth, cellNum)
 }
