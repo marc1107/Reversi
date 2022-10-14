434618d45fe44876c77aee743002b1b83d9cc9eb
marc1107 <m.bohner99@gmail.com>
2022-10-14 10:12:04 +0200

    moved to package de.htwg

--- src/main/scala/de/htwg/Reversi.scala
+++ src/main/scala/de/htwg/Reversi.scala
@@ -1,17 +1,19 @@
+package de.htwg
+
 object Reversi {
+  val eol: String = sys.props("line.separator")
+
   def main(args: Array[String]): Unit = {
     println("Welcome to Reversi")
     println(mesh(cellNum = 2))
   }
 
-  val eol: String = sys.props("line.separator")
+  def mesh(cellWidth: Int = 4, cellNum: Int = 8): String =
+    (bar(cellWidth, cellNum) + cells(cellWidth, cellNum)) * cellNum + bar(cellWidth, cellNum)
 
   def bar(cellWidth: Int = 4, cellNum: Int = 8): String =
     ("+" + "-" * cellWidth) * cellNum + "+" + eol
 
   def cells(cellWidth: Int = 4, cellNum: Int = 8): String =
     ("|" + " " * cellWidth) * cellNum + "|" + eol
-
-  def mesh(cellWidth: Int = 4, cellNum: Int = 8): String =
-    (bar(cellWidth, cellNum) + cells(cellWidth, cellNum)) * cellNum + bar(cellWidth, cellNum)
 }
