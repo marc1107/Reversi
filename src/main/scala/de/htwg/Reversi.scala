98f8523215b99f49c5dc2a807c38d12cef23946e
marc1107 <m.bohner99@gmail.com>
2022-10-21 09:41:56 +0200

    changed mesh cellNum to 9

--- src/main/scala/de/htwg/Reversi.scala
+++ src/main/scala/de/htwg/Reversi.scala
@@ -8,7 +8,7 @@ object Reversi {
     println(mesh(cellNum = 2))
   }
 
-  def mesh(cellWidth: Int = 4, cellNum: Int = 8): String =
+  def mesh(cellWidth: Int = 4, cellNum: Int = 9): String =
     (bar(cellWidth, cellNum) + cells(cellWidth, cellNum)) * cellNum + bar(cellWidth, cellNum)
 
   def bar(cellWidth: Int = 4, cellNum: Int = 8): String =
