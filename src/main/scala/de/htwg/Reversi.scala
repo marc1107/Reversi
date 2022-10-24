bbf4c83b28faaf1fdcdd18738c1cefe579fc42b9
marc1107 <m.bohner99@gmail.com>
2022-10-24 17:23:21 +0200

    Changed default cellNum to 4

--- src/main/scala/de/htwg/Reversi.scala
+++ src/main/scala/de/htwg/Reversi.scala
@@ -5,16 +5,16 @@ object Reversi {
 
   def main(args: Array[String]): Unit = {
     println("Welcome to Reversi")
-    println(mesh(cellNum = 2))
+    println(mesh(cellNum = 8))
   }
 
-  def mesh(cellWidth: Int = 4, cellNum: Int = 8): String =
+  def mesh(cellWidth: Int = 4, cellNum: Int = 4): String =
 
     (bar(cellWidth, cellNum) + cells(cellWidth, cellNum)) * cellNum + bar(cellWidth, cellNum)
 
-  def bar(cellWidth: Int = 4, cellNum: Int = 8): String =
+  def bar(cellWidth: Int = 4, cellNum: Int = 4): String =
     ("+" + "-" * cellWidth) * cellNum + "+" + eol
 
-  def cells(cellWidth: Int = 4, cellNum: Int = 8): String =
+  def cells(cellWidth: Int = 4, cellNum: Int = 4): String =
     ("|" + " " * cellWidth) * cellNum + "|" + eol
 }
