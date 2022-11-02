d13dc7f33a9f6012c16213e899274f79ecc0b4c8
marc1107 <m.bohner99@gmail.com>
2022-11-02 21:43:41 +0100

    added methods isMovePossible(), makeMove() and askForNextMove() and their tests

--- src/main/scala/de/htwg/Reversi.scala
+++ src/main/scala/de/htwg/Reversi.scala
@@ -1,5 +1,7 @@
 package de.htwg
 
+import scala.io.StdIn._
+
 object Reversi {
   val eol: String = sys.props("line.separator")
 
@@ -16,4 +18,53 @@ object Reversi {
 
   def cells(cellWidth: Int = 4, cellNum: Int = 4): String =
     ("|" + " " * cellWidth) * cellNum + "|" + eol
+
+  /**
+   * checks if the requested move is possible
+   * @param arr game state array
+   * @param x x coordinate
+   * @param y y coordinate
+   * @return true if the move is possible, false if not possible
+   */
+  def isMovePossible(arr: Array[Array[Int]], x: Int, y: Int): Boolean = {
+    if (x > arr.length || y > arr(0).length) {
+      false
+    } else {
+      arr(x-1)(y-1) == 0
+    }
+  }
+
+  /**
+   * Makes a move for the player in the game state array
+   * @param arr game state array
+   * @param x x coordinate
+   * @param y y coordinate
+   * @param player the player whose turn it is
+   * @return game state array including the players move
+   */
+  def makeMove(arr: Array[Array[Int]], x: Int, y: Int, player: Int): Array[Array[Int]] = {
+    if (isMovePossible(arr, x, y)) {
+      arr(x-1)(y-1) = player
+    }
+    arr
+  }
+
+  /**
+   * Asks the player whose turn it is to input his move
+   * @param player the player whose turn it is
+   * @param names an Array with the size 2 which contains the Names of the 2 players
+   * @return Array with the size 2 which contains the x and y coordinate the player choose
+   */
+  def askForNextMove(player: Int, names: Array[String]): Array[Int] = {
+    if (names.length < 2)
+      throw new Exception("2 Players required")
+
+    println(names(player-1) + " ist an der Reihe")
+    println("x eingeben:")
+    val x = readInt()
+    println("y eingeben:")
+    val y = readInt()
+    val move = Array(x, y)
+    move
+  }
 }
--- src/test/scala/de/htwg/ReversiSpec.scala
+++ src/test/scala/de/htwg/ReversiSpec.scala
@@ -14,5 +14,20 @@ class ReversiSpec extends AnyWordSpec {
     "have mesh as String of form '+----+----+\n|    |    |\n+----+----+\n|    |    |\n+----+----+'" in {
       Reversi.mesh() should be("+----+----+" + Reversi.eol + "|    |    |" + Reversi.eol + "+----+----+" + Reversi.eol + "|    |    |" + Reversi.eol + "+----+----+" + Reversi.eol)
     }
+    "have value as Boolean of form 'true'" in {
+      Reversi.isMovePossible(Array(Array(0, 1), Array(2, 1)), 1, 1) should be(true)
+    }
+    "have value as Boolean of form 'false'" in {
+      Reversi.isMovePossible(Array(Array(0, 1), Array(2, 1)), 2, 1) should be(false)
+    }
+    "have game state as Array of form 'Array((2,1),(2,1))'" in {
+      Reversi.makeMove(Array(Array(0, 1), Array(2, 1)), 1, 1, 2) should be(Array(Array(2,1), Array(2,1)))
+    }
+    "have game state as Array of form 'Array((0,1),(2,1))'" in {
+      Reversi.makeMove(Array(Array(0, 1), Array(2, 1)), 2, 1, 2) should be(Array(Array(0,1), Array(2,1)))
+    }
+    "have move as Array of form 'Array(0, 0)'" in {
+      Reversi.askForNextMove(1, Array("Lukas", "Marc")) should be(Array(0, 0))
+    }
   }
 }
