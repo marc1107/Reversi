e1368292ac6cfdca964ea323240e2b02f3452a13
marc1107 <m.bohner99@gmail.com>
2022-11-11 10:25:14 +0100

    small changes

--- src/main/scala/de/htwg/util/Observer.scala
+++ src/main/scala/de/htwg/util/Observer.scala
@@ -1,11 +1,16 @@
 package de.htwg
 package util
 
-trait Observer:
+trait Observer {
   def update: Unit
+}
 
-trait Observable:
+class Observable {
   var subscribers: Vector[Observer] = Vector()
+
   def add(s: Observer) = subscribers = subscribers :+ s
+
   def remove(s: Observer) = subscribers = subscribers.filterNot(o => o == s)
+
   def notifyObservers = subscribers.foreach(o => o.update)
+}
