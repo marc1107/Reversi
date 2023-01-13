7f65fe0555cc6f9c6fcc2dfe175ea89644a4f8c0
marc1107 <m.bohner99@gmail.com>
2023-01-13 11:16:14 +0100

    optimized imports

--- src/main/scala/de/htwg/model/fileIoComponent/fileIoJsonImpl/FileIO.scala
+++ src/main/scala/de/htwg/model/fileIoComponent/fileIoJsonImpl/FileIO.scala
@@ -1,14 +1,9 @@
 package de.htwg
 package model.fileIoComponent.fileIoJsonImpl
 
-//import com.google.inject.Guice
-//import com.google.inject.name.Names
-//import net.codingwell.scalaguice.InjectorExtensions._
-//import de.htwg.se.sudoku.SudokuModule
 import de.htwg.model.Stone
 import model.fieldComponent.{Field, FieldInterface}
 import model.fileIoComponent.FileIOInterface
-//import de.htwg.se.sudoku.model.gridComponent.{ CellInterface, GridInterface }
 import play.api.libs.json._
 
 import scala.io.Source
@@ -24,7 +19,6 @@ class FileIO extends FileIOInterface {
     for (index <- 0 until size * size) {
       val row = (json \\ "row")(index).as[Int]
       val col = (json \\ "col")(index).as[Int]
-      //val cell = (json \\ "cell")(index)
       val value = (json \\ "cell")(index).as[String]
       value match {
         case " " => field = field.put(Stone.Empty, row, col)
@@ -42,14 +36,6 @@ class FileIO extends FileIOInterface {
     pw.write(Json.prettyPrint(fieldToJson(field)))
     pw.close
   }
-
-  /*implicit val stoneWrites = new Writes[Stone] {
-    def writes(stone: Stone) = Json.obj(
-      "value" -> stone.toString)/*,
-      "given" -> cell.given,
-    "showCandidates" -> cell.showCandidates
-    )*/
-  }*/
 
   def fieldToJson(field: FieldInterface) = {
     Json.obj(
