160bdd64681c6b47d4496999f67dd36fcf46c377
marc1107 <m.bohner99@gmail.com>
2023-01-21 16:33:55 +0100

    edited tests

--- src/main/scala/de/htwg/model/Stone.scala
+++ src/main/scala/de/htwg/model/Stone.scala
@@ -2,6 +2,6 @@ package de.htwg.model
 
 enum Stone(stringRepresentation: String):
   override def toString: String = stringRepresentation
-  case B extends Stone("\u25A1")
-  case W extends Stone("\u25A0")
+  case W extends Stone("\u25A1")
+  case B extends Stone("\u25A0")
   case Empty extends Stone(" ")
--- src/test/scala/de/htwg/controller/ControllerSpec.scala
+++ src/test/scala/de/htwg/controller/ControllerSpec.scala
@@ -35,7 +35,7 @@ class ControllerSpec extends AnyWordSpec {
     }
     "print a field" in {
       controller.toString should be( """#+---+---+---+
-                                        #| □ | □ | □ |
+                                        #| ■ | ■ | ■ |
                                         #+---+---+---+
                                         #|   |   |   |
                                         #+---+---+---+
