c46e5433117cecc0cc93cd5140c3b2d2515dacd8
marc1107 <m.bohner99@gmail.com>
2024-03-27 10:44:13 +0100

    refactore Default class

--- src/main/scala/de/htwg/Default.scala
+++ src/main/scala/de/htwg/Default.scala
@@ -7,11 +7,11 @@ import de.htwg.model.fileIoComponent.FileIOInterface
 import de.htwg.model.fileIoComponent.fileIoXmlImpl.FileIO
 
 object Default {
-  var field = new Field(8, Stone.Empty)
-  field = field.put(Stone.W, 4, 4)
-  field = field.put(Stone.B, 4, 5)
-  field = field.put(Stone.B, 5, 4)
-  field = field.put(Stone.W, 5, 5)
+  private val baseField = new Field(8, Stone.Empty)
+  val field1: Field = baseField.put(Stone.W, 4, 4)
+  val field2: Field = field1.put(Stone.B, 4, 5)
+  val field3: Field = field2.put(Stone.B, 5, 4)
+  val field: Field = field3.put(Stone.W, 5, 5)
 
   given FieldInterface = field
 
