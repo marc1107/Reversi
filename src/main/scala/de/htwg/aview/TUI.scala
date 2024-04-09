d1317073d3707af2e1ea158fcba01e4c2823dfe7
marc1107 <m.bohner99@gmail.com>
2024-04-09 14:55:04 +0200

    using Try monad with Success and Failure to analyse the input and prevent crashes

--- src/main/scala/de/htwg/aview/TUI.scala
+++ src/main/scala/de/htwg/aview/TUI.scala
@@ -6,9 +6,10 @@ import de.htwg.model.Move
 import de.htwg.util.Event
 
 import scala.io.StdIn.readLine
+import scala.util.{Failure, Success, Try}
 
 class TUI(using controller: ControllerInterface) extends UI(controller):
-  override def update(e: Event) = e match {
+  override def update(e: Event): Unit = e match {
     case Event.Quit => sys.exit()
     case Event.Move =>
       println(controller.playerState.getStone.toString + " ist an der Reihe")
@@ -26,19 +27,29 @@ class TUI(using controller: ControllerInterface) extends UI(controller):
   /**
    * analyses the input from teh console and calls the controller
    *
-   * @param input String
+   * @param consoleIn console input
    * @return Option (Some(Move) or None)
    */
-  override def analyseInput(input: String): Option[Move] =
+  override def analyseInput(consoleIn: String): Option[Move] =
+    val input = inputSuccess(consoleIn)
     input match
-      case "q" => sys.exit()
-      case "u" => controller.doAndPublish(controller.undo); None
-      case "r" => controller.doAndPublish(controller.redo); None
-      case "s" => controller.doAndPublish(controller.save); None
-      case "l" => controller.doAndPublish(controller.load); None
-      case _ =>
-        val chars = input.toCharArray
-        val stone = controller.playerState.getStone
-        val r = chars(0).toString.toInt
-        val c = chars(1).toString.toInt
-        Some(Move(stone, r, c))
+      case Success(value) =>
+        value match
+          case "q" => sys.exit()
+          case "u" => controller.doAndPublish(controller.undo); None
+          case "r" => controller.doAndPublish(controller.redo); None
+          case "s" => controller.doAndPublish(controller.save); None
+          case "l" => controller.doAndPublish(controller.load); None
+          case _ =>
+            val chars = value.toCharArray
+            val stone = controller.playerState.getStone
+            val r = chars(0).toString.toInt
+            val c = chars(1).toString.toInt
+            Some(Move(stone, r, c))
+      case Failure(error) => println(error.getMessage); None
+
+  private def inputSuccess(input: String): Try[String] =
+    val pattern = "^(q|u|r|s|l|[1-8][1-8])$".r
+    input match
+      case pattern(_*) => Success(input)
+      case _ => Failure(IllegalArgumentException("Invalid input"))
