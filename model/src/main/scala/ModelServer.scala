ceff892a77c374e88151ed120613ba2540e169bc
marc1107 <m.bohner99@gmail.com>
2024-04-17 16:41:36 +0200

    using port val

--- model/src/main/scala/ModelServer.scala
+++ model/src/main/scala/ModelServer.scala
@@ -12,6 +12,7 @@ object ModelServer {
   def main(args: Array[String]): Unit = {
     implicit val system: ActorSystem = ActorSystem("mySystem")
     implicit val executionContext: ExecutionContext = system.dispatcher
+    val port = 8080
 
     val gameField: FieldInterface = field
 
@@ -19,9 +20,9 @@ object ModelServer {
     val routes: Route = fieldApi.routes
 
     // Start the server
-    val bindingFuture = Http().newServerAt("localhost", 8080).bind(routes)
+    val bindingFuture = Http().newServerAt("localhost", port).bind(routes)
 
-    println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+    println(s"Server online at http://localhost:$port/\nPress RETURN to stop...")
     StdIn.readLine() // let it run until user presses return
     bindingFuture
       .flatMap(_.unbind()) // trigger unbinding from the port
