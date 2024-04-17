a6a4251e8302024d24d826eb2b8bbe8ad1f8af27
marc1107 <m.bohner99@gmail.com>
2024-04-17 16:06:21 +0200

    renamed to fit module naming

--- model/src/main/scala/ModuleServer.scala
+++ model/src/main/scala/ModuleServer.scala
@@ -2,20 +2,20 @@ import CoreModule.field
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.server.Route
-import api.FieldApi
+import api.ModuleApi
 import fieldComponent.FieldInterface
 
 import scala.concurrent.ExecutionContext
 import scala.io.StdIn
 
-object WebServer {
+object ModuleServer {
   def main(args: Array[String]): Unit = {
     implicit val system: ActorSystem = ActorSystem("mySystem")
     implicit val executionContext: ExecutionContext = system.dispatcher
 
     val gameField: FieldInterface = field
 
-    val fieldApi = new FieldApi(gameField)
+    val fieldApi = new ModuleApi(gameField)
     val routes: Route = fieldApi.routes
 
     // Start the server
--- model/src/main/scala/api/ModuleApi.scala
+++ model/src/main/scala/api/ModuleApi.scala
@@ -6,7 +6,7 @@ import fieldComponent.{FieldInterface, Stone}
 import org.slf4j.{Logger, LoggerFactory}
 import play.api.libs.json.{JsValue, Json}
 
-class FieldApi(var field: FieldInterface) {
+class ModuleApi(var field: FieldInterface) {
   val log: Logger = LoggerFactory.getLogger(getClass)
 
   val routes: Route = pathPrefix("field") {
